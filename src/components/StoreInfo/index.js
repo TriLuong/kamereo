1fe537bb7df2f218e19ae7712763786c5630ea65
Tri Luong <lxt0094@gmail.com>
2019-05-08 00:45:10 +0700

    Add button Edit Profile for project.
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

--- src/components/StoreInfo/index.js
+++ src/components/StoreInfo/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function StoreInfo() {
+  const showEditStoreProfile = () => {
+    console.log("Click Edit Profile");
+  };
   return (
     <>
       <h1>Store Infomation</h1>
 
-      <div class="row">
-        <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 store-info">
-          <img src="#" class="img-responsive" alt="Avatar" />
+      <div className="row">
+        <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5 store-info">
+          <img src="#" className="img-responsive" alt="Avatar" />
           <p>STORE INFO</p>
           <ul>
             <li>Name</li>
@@ -20,8 +23,17 @@ function StoreInfo() {
             <li>Address</li>
             <li>Tax Number</li>
           </ul>
+
+          <button
+            type="button"
+            className="btn btn-success"
+            style={{ marginBottom: "1em" }}
+            onClick={showEditStoreProfile}
+          >
+            Edit Profile
+          </button>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 delivery-message">
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 delivery-message">
           <p>DELIVERY DEFAULT MESSAGE</p>
         </div>
       </div>
